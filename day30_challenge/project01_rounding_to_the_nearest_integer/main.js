5ff311ff8b0a8632d32a11a2769265f8057f277e
Uzair Ahmed <159642641+Uzair0011@users.noreply.github.com>
2024-04-01 23:37:50 +0500

    Add files via upload

---
+++ day30_challenge/project01_rounding_to_the_nearest_integer/main.js
@@ -0,0 +1,11 @@
+// *Question 88:* Rounding to the Nearest Integer: Write a function that
+//  takes a decimal number as input and returns the number rounded to the
+//  nearest integer.
+// *Explain & TIP:* The Math.round() function is perfect for rounding
+// decimals to the closest whole number. It looks at the decimal part and
+//  decides whether to round up or down.
+function roundToNearestInteger(num) {
+    return Math.round(num);
+}
+console.log(roundToNearestInteger(9.2));
+console.log(roundToNearestInteger(6.8));
