89853c3d197d92a65247f077aa82eab2a70e6bd0
Uzair Ahmed <159642641+Uzair0011@users.noreply.github.com>
2024-04-11 12:02:19 +0500

    Add files via upload

---
+++ day36_challenge/project02_condition_using_logical_operator/main.js
@@ -0,0 +1,11 @@
+// *Question 107:* Write a condition using logical operators
+//  that checks if a number is divisible by both 2 and 3.
+// *Explain & TIP:* Checking for divisibility involves using
+//  the modulo operator (%) to see if there's any remainder.
+//   A number divisible by both 2 and 3 without a remainder is
+//   also divisible by 6.
+function isDivisibleByTwoAndThree(number) {
+    return number % 2 === 0 && number % 3 === 0;
+}
+console.log(isDivisibleByTwoAndThree(12));
+console.log(isDivisibleByTwoAndThree(14));
---
+++ day36_challenge/project03_string_identical_check/main.ts
@@ -0,0 +1,11 @@
+// *Question 108:* Compare two strings to check if they are 
+// identical, ignoring case sensitivity.
+// *Explain & TIP:* To compare strings without considering
+//  their case (uppercase/lowercase), you can convert both
+//  strings to the same case before comparing.
+function areStringsEqualIgnoreCase(str1: string,
+    str2: string): boolean {
+    return str1.toLowerCase() === str2.toLowerCase();
+  }
+  console.log(areStringsEqualIgnoreCase("hello", "Hello"));
+  console.log(areStringsEqualIgnoreCase("world", "Word")); 
