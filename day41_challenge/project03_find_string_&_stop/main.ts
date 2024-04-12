e22b7f54819587d75947041656a36db9971cf6e9
Uzair Ahmed <159642641+Uzair0011@users.noreply.github.com>
2024-04-12 16:06:31 +0500

    Add files via upload

---
+++ day41_challenge/project03_find_string_&_stop/main.ts
@@ -0,0 +1,20 @@
+// *Question 123:* Create a loop that iterates through a
+//  string and stops when it finds the first vowel.
+// *Explain & TIP:* To find a specific character in a string,
+// you can iterate through each character and use a condition
+// to check for vowels. The loop can stop once a vowel is 
+// found.
+
+function logUntilVowel(str: string): void {
+    const vowels = "aeiouAEIOU";
+    for (const char of str) {
+      if (vowels.includes(char)) {
+        console.log(`First vowel found: ${char}`);
+        break; 
+      }
+      console.log(char); 
+    }
+  }
+  
+  logUntilVowel("syzygy");
+
