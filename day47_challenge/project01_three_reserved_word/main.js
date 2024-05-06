17d30d62c11d31807bae4193c8f80ef5e1c0fd4c
Uzair Ahmed <159642641+Uzair0011@users.noreply.github.com>
2024-05-06 11:04:07 +0500

    Add files via upload

---
+++ day47_challenge/project01_three_reserved_word/main.js
@@ -0,0 +1,9 @@
+// *Question 139:* List three reserved words in JavaScript and create a valid use case for each.
+// *Explain & TIP:* Reserved words in JavaScript are words that have a special meaning and cannot be used as identifiers (e.g., variable names, function names). Understanding these can help prevent syntax errors and make your code more readable.
+var count = 10;
+if (count > 0) {
+    console.log("Count is greater than 0.");
+}
+function add(a, b) {
+    return a + b;
+}
---
+++ day47_challenge/project01_three_reserved_word/main.ts
@@ -0,0 +1,15 @@
+// *Question 139:* List three reserved words in JavaScript and create a valid use case for each.
+
+// *Explain & TIP:* Reserved words in JavaScript are words that have a special meaning and cannot be used as identifiers (e.g., variable names, function names). Understanding these can help prevent syntax errors and make your code more readable.
+
+
+let count = 10;
+
+
+if (count > 0) {
+  console.log("Count is greater than 0.");
+}
+
+function add(a, b) {
+  return a + b;
+}
